e55e4fffe375ca36ab98b96bbfe30d51d54d9258
DreadfulCrazy <lucasospina97@gmail.com>
2022-10-19 20:59:52 -0500

    SampleScene deleted and level1 created for SceneManagement reload

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
@@ -8,7 +9,8 @@ public class FinishLine : MonoBehaviour
     {
         if (collision.tag == "Player")
         {
-        Debug.Log("Finished");
+            SceneManager.LoadScene(0);
+
         }
     }
 }
